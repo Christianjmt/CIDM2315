4f7b9d163ec103a1b46f9c0333c0620e10adaeb7
Christianjmt <143286639+Christianjmt@users.noreply.github.com>
2023-08-26 13:04:40 -0500

    Add files via upload

---
+++ Homework_1.cs
@@ -0,0 +1,15 @@
+﻿namespace CIDM_Proj_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float x = 2.5f;
+        float y = 3.3f;
+        float z = (4*(x*x) + 3*y);
+
+        Console.WriteLine("X = {0}, Y = {1}", x,y);
+        Console.Write("The Value of Z is: {0} " , z);
+        
+    }
+}
