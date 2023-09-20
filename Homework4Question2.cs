7332e96a0989cec5e79c4a827ed9cfd7748df946
Christianjmt <143286639+Christianjmt@users.noreply.github.com>
2023-09-20 13:03:52 -0500

    Add files via upload

---
+++ Homework4Question2.cs
@@ -0,0 +1,53 @@
+﻿namespace HW4Q2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Assign an int value to N: "); 
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Left align or right align?(input left or right): ");
+        string b = Console.ReadLine();
+        Console.WriteLine($"N is {a}; Shape is {b}");
+        input(a,b);
+        
+        
+    static void input (int a, string b)
+    {
+       int c = a-1;
+       if(b=="left" || b == "Left")
+       { 
+        for(int row = 0; row<a; row++)
+            {
+                for(int col = 0; col<a; col++)
+                {
+                    if(row>=col)
+                    {
+                    Console.Write('*');
+                    }
+                }
+        Console.WriteLine("");
+            }
+        }
+        else if(b=="right" || b == "Right")
+        {
+            for(int row = 0; row<a; row++)
+            {
+                for(int spaces = 0; spaces <= c ; spaces++)
+                {
+                    Console.Write(" ");
+                }
+                for(int col = 0; col<a; col++)
+                {
+                    if(row>=col)
+                    {
+                        Console.Write('*');
+                    }
+                }
+                Console.WriteLine("");
+                c--;
+            }
+        }
+    }
+    }
+}
---
+++ Homework4question1.cs
@@ -0,0 +1,26 @@
+﻿namespace HW4Q1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please input two integers: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        input(a,b);
+    }
+    static void input(int a, int b)
+    { 
+        Console.WriteLine($"a = {a}; b = {b} ");
+        if(a>b)
+        {
+            Console.WriteLine($"The largest number is:{a}");
+            
+        }
+        else
+        {
+            Console.WriteLine($"The largest number is:{b}");
+            
+        }
+    }
+}
